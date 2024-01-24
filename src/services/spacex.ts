f8bc853317742e9a6739f8e70cd0592eef2a3451
PiedraL <joaquinlangone12@gmail.com>
2024-01-24 16:15:24 -0300

    everything working properly

--- src/services/spacex.ts
+++ src/services/spacex.ts
@@ -1,27 +1,40 @@
-import { type APISpaceX } from "../types/api"
+import { type Doc, type APISpaceX } from "../types/api"
 
 export const getLatestLaunches = async () => {
-    
-    const URL = "https://api.spacexdata.com/v5/launches/query"
 
-    const response = await fetch(URL ,{
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            query: {},
-            options: {
-                sort: {
-                    date_unix: "asc",
-                },
-                limit: 12,
+    try{
+        const URL = "https://api.spacexdata.com/v5/launches/query"
+
+        const response = await fetch(URL ,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
             },
-        }),
-    })   
-    
-    const {docs: launches} = (await response.json()) as APISpaceX
-    return launches
+            body: JSON.stringify({
+                query: {},
+                options: {
+                    sort: {
+                        date_unix: "asc",
+                    },
+                    limit: 120,
+                },
+            }),
+        })
+        
+
+        if(!response.ok){
+            throw new Error(`Failed to fetch data. Status: ${response.status}`)
+        }
+
+        const {docs: launches} = (await response.json()) as APISpaceX
+        console.log(launches);
+        return launches
+
+        
+    }catch(error){
+        console.error(`Error fetching data:`, error)
+        throw error
+    }
 }
 
 // this for a more readable Code, separing the fetching in a ts or js file and
@@ -30,12 +43,19 @@ export const getLatestLaunches = async () => {
 // ------
 
 // Now we fetch the id 
-// export const getLaunchesId = async ({id}) => {
+export const getLaunchesId = async ({id}: {id: string}) => {
     
-//     const URL = ` https://api.spacexdata.com/v5/launches/${id}`
+    try{
+        const URL = ` https://api.spacexdata.com/v5/launches/${id}`
+        const response = await fetch(URL)
 
-   
-    
-//     const {docs: launches} = (await response.json()) as APISpaceX
-//     return launches
-// }
+        if(!response.ok){
+            throw new Error(`Failed to fetch data. Status: ${response.status}`)
+        }
+        const launch = (await response.json()) as Doc
+        return launch
+    }catch(error){
+        console.error(`Error fetching data:`, error)
+        throw error
+    }
+}
